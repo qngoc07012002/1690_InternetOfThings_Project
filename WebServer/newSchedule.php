aa6bea859e42cf534a9644aa8c6e3221d13fae17
qngoc07012002 <116235732+qngoc07012002@users.noreply.github.com>
2023-07-25 05:34:03 +0700

    Update newSchedule.php

--- WebServer/newSchedule.php
+++ WebServer/newSchedule.php
@@ -1,6 +1,6 @@
 <?php
 
-// Kết nối đến cơ sở dữ liệu MySQL
+
 $servername = "103.27.239.251"; 
 $username = "ngoc_db"; 
 $password = "ngoc123";
@@ -8,36 +8,34 @@ $dbname = "ngoc_db";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Kiểm tra kết nối
+
 if ($conn->connect_error) {
     die("Kết nối đến cơ sở dữ liệu thất bại: " . $conn->connect_error);
 }
 
-// Lấy ngày hiện tại
+
 $currentDate = date("Y-m-d");
 
-// Truy vấn lấy tất cả RFID mà Student_Code không phải NULL
+
 $sql = "SELECT RFID FROM Student WHERE Student_Code IS NOT NULL";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-    // Lặp qua kết quả và thêm vào bảng Attendance
+    
     while ($row = $result->fetch_assoc()) {
         $rfid = $row["RFID"];
         $status = "NY";
 
-        // Truy vấn kiểm tra xem bản ghi đã tồn tại hay chưa
         $checkQuery = "SELECT RFID FROM Attendance WHERE RFID = '$rfid' AND Date = '$currentDate'";
         $checkResult = $conn->query($checkQuery);
 
         if ($checkResult->num_rows > 0) {
             echo "Bản ghi đã tồn tại cho sinh viên có RFID: $rfid và ngày $currentDate<br>";
         } else {
-            // Truy vấn để thêm bản ghi vào bảng Attendance
             $insertQuery = "INSERT INTO Attendance (RFID, Status, Date) VALUES ('$rfid', '$status', '$currentDate')";
 
             if ($conn->query($insertQuery) === TRUE) {
-                echo "Đã tạo lịch điểm danh cho sinh viên có RFID: $rfid và ngày $currentDate<br>";
+                echo "Đã tạo lịch điểm danh cho sinh viên có RFID: $rfid vào ngày $currentDate<br>";
             } else {
                 echo "Lỗi: " . $insertQuery . "<br>" . $conn->error;
             }
@@ -47,7 +45,6 @@ if ($result->num_rows > 0) {
     echo "Không tìm thấy sinh viên nào có mã RFID";
 }
 
-// Đóng kết nối
 $conn->close();
 
 ?>
