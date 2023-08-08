8f3c60dfc0f95263d713f53dd49056204d389c95
qngoc07012002 <116235732+qngoc07012002@users.noreply.github.com>
2023-08-08 17:14:01 +0700

    Update get_StudentInfomation.php

--- WebServer/get_StudentInfomation.php
+++ WebServer/get_StudentInfomation.php
@@ -4,29 +4,25 @@ $username = "ngoc_db";
 $password = "ngoc123";
 $dbname = "ngoc_db"; 
 
-
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
     die("Connect SQL Fail: " . $conn->connect_error);
 }
 
-if (isset($_GET['rfid'])) {
-    $rfid = $_GET['rfid'];
+$sql = "SELECT * FROM Student";
+$result = $conn->query($sql);
 
-    $sql = "SELECT * FROM Student WHERE RFID = '$rfid'";
-    $result = $conn->query($sql);
-
-    if ($result->num_rows > 0) {
-        $student = $result->fetch_assoc();
-
-        header('Content-Type: application/json');
-        echo json_encode($student);
-    } else {
-        echo json_encode(array("error" => "Student Not Found."));
+if ($result->num_rows > 0) {
+    $students = array();
+    while ($row = $result->fetch_assoc()) {
+        $students[] = $row;
     }
+
+    header('Content-Type: application/json');
+    echo json_encode($students);
 } else {
-    echo json_encode(array("error" => "RFID Null"));
+    echo json_encode(array("error" => "No Students Found."));
 }
 
 $conn->close();
